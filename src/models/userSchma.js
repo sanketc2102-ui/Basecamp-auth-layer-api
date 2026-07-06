c1e20f696072bd8c89f71769ff1ff26fd6f5e1f1
Sanket Chavan <sanketc2102@gmail.com>
2026-07-06 20:10:35 +0530

    feat(auth): add user schema in mongoDB

---
+++ src/models/userSchma.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPth: String,
+      },
+      default: {
+        url: "https://placehold.co/200x200",
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      require: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    emailVerificationToken: {
+      type: String,
+    },
+
+    emailVerificationExpiery: {
+      type: Date,
+    },
+
+    forgetPasswordToken: {
+      type: String,
+    },
+
+    forgetPasswordExpiery: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
